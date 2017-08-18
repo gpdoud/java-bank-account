8fab3e5e549ee644f185f6e2df61e9f44217b054
Greg doud <gpdoud@gmail.com>
2017-08-18 16:09:32 -0400

    Added savings.java class

--- src/com/maxtrain/Account.java
+++ src/com/maxtrain/Account.java
@@ -1,15 +1,47 @@
 package com.maxtrain;
 
 public class Account {
+	static int nextNbr = 1;
 	// all the properties
 	private double balance; // holds the amount in the account
 	private String nbr; // account number
 	private String desc; // user-defined name
 	
-	public Account(String nbr, String desc) {
+	public String getNbr() {
+		return this.nbr;
+	}
+	private void setNbr(String nbr) {
 		this.nbr = nbr;
+	}
+	public String getDesc() {
+		return this.desc;
+	}
+	public void setDesc(String desc) {
 		this.desc = desc;
-		this.balance = 0;
+	}
+	public double getBalance() {
+		return this.balance;
+	}
+	private void setBalance(double balance) {
+		this.balance = balance;
+	}
+	// constructor
+	public Account(String desc) {
+		this.setNbr(String.valueOf(nextNbr));
+		nextNbr++;
+		//this.desc = desc;
+		this.setDesc(desc);
+		this.setBalance(0);
+	}
+	
+	public Boolean transfer(double amount, Account toAccount) {
+		if(this.getBalance() < amount || amount <= 0) {
+			return false;
+		}
+		this.withdraw(amount);
+		toAccount.deposit(amount);
+		System.out.println("Successful transfer of " + amount);
+		return true;
 	}
 	
 	public double deposit(double amount) {
@@ -32,5 +64,12 @@ public class Account {
 		}
 		this.balance -= amount;
 		return this.balance;
+	}
+	
+	public String display() {		
+		return "Account: nbr is " + this.getNbr() 
+				+ ", desc is " + this.getDesc() 
+				+ ", balance is " + this.getBalance();
+		
 	}
 }
--- src/com/maxtrain/Mainline.java
+++ src/com/maxtrain/Mainline.java
@@ -4,7 +4,18 @@ public class Mainline {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Account checking = new Account("100", "My First Checking");
+		Savings savings = new Savings("My First Savings");
+		savings.deposit(1000.00);
+		double interest = savings.payInterest(3);
+		System.out.println(savings.display());
+
+		Account checking = new Account("My First Checking");
+		
+		
+		checking.setDesc("Greg's Checking");
+		
+		System.out.println(checking.display());
+		System.out.println(savings.display());
 		
 		double newBalance = checking.deposit(100.00);
 		System.out.println("Deposit $100: Balance is " + newBalance);
@@ -24,6 +35,9 @@ public class Mainline {
 		newBalance = checking.withdraw(250.00);
 		System.out.println("Withdraw $250: Balance is " + newBalance);
 
+		savings.transfer(200.0, checking);
+		System.out.println(checking.display());
+		System.out.println(savings.display());
 	}
 
 }
